17d94b634514dbf8b60cccddac00fce124f5a717
Luciano Santos <pdform@gmail.com>
2016-06-24 12:23:06 +0900

    gambiarra p/ luisa

--- src/org/unbiquitous/unbihealth/fisiogame/InputController.java
+++ src/org/unbiquitous/unbihealth/fisiogame/InputController.java
@@ -357,7 +357,7 @@ public class InputController extends JFrame {
 
     private void txtIPChanged() {
         final Pattern HOST_NAME = Pattern.compile(
-                "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+                "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\:[0-9]{1,5})?$");
         final Color NORMAL = UIManager.getDefaults().getColor("TextField.foregroundColor");
         final Color ERROR = Color.ORANGE;
 
